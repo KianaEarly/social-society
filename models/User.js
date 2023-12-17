8ae862c30e54644aa89db9022d8fb331b8cc2049
Bubble <kiana.sambursky@gmail.com>
2023-12-17 14:04:28 -0600

    added models

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
